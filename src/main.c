549eb2f3033f29d9fa9290cdb19daebbc344ff73
Zack Kiener <Zgame0077@gmail.com>
2024-02-29 15:14:51 -0500

    updated

--- src/main.c
+++ src/main.c
@@ -1,82 +1,182 @@
-#include <stm32f446xx.h>
+#include <stm32f4xx_hal.h>
 
-#define MODIFY_FIELD(reg, field, value)  \
-    ((reg)) = ((reg) & ~(field##_Msk)) | \
-              (((uint32_t)(value) << field##_Pos) & field##_Msk)
+#define USER_Btn_Pin GPIO_PIN_13
+#define USER_Btn_GPIO_Port GPIOC
+#define MCO_Pin GPIO_PIN_0
+#define MCO_GPIO_Port GPIOH
+#define LD1_Pin GPIO_PIN_0
+#define LD1_GPIO_Port GPIOB
+#define LD3_Pin GPIO_PIN_14
+#define LD3_GPIO_Port GPIOB
+#define STLK_RX_Pin GPIO_PIN_8
+#define STLK_RX_GPIO_Port GPIOD
+#define STLK_TX_Pin GPIO_PIN_9
+#define STLK_TX_GPIO_Port GPIOD
+#define USB_PowerSwitchOn_Pin GPIO_PIN_6
+#define USB_PowerSwitchOn_GPIO_Port GPIOG
+#define USB_OverCurrent_Pin GPIO_PIN_7
+#define USB_OverCurrent_GPIO_Port GPIOG
+#define USB_SOF_Pin GPIO_PIN_8
+#define USB_SOF_GPIO_Port GPIOA
+#define USB_VBUS_Pin GPIO_PIN_9
+#define USB_VBUS_GPIO_Port GPIOA
+#define USB_ID_Pin GPIO_PIN_10
+#define USB_ID_GPIO_Port GPIOA
+#define USB_DM_Pin GPIO_PIN_11
+#define USB_DM_GPIO_Port GPIOA
+#define USB_DP_Pin GPIO_PIN_12
+#define USB_DP_GPIO_Port GPIOA
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define LD2_Pin GPIO_PIN_7
+#define LD2_GPIO_Port GPIOB
 
-void Init_SPI1(void)
-{
-    // Clock gating for SPI1 and GPIOA and B
-    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN;
+SPI_HandleTypeDef hspi1;
 
-    // GPIO B pin 1, 8, 13, 15 to NSS
-    MODIFY_FIELD(GPIOB->MODER, GPIO_MODER_MODER1, 2);
-    MODIFY_FIELD(GPIOB->MODER, GPIO_MODER_MODER8, 2);
-    MODIFY_FIELD(GPIOB->MODER, GPIO_MODER_MODER13, 2);
-    MODIFY_FIELD(GPIOB->MODER, GPIO_MODER_MODER15, 2);
-
-    // GPIO A pin 1, 11, 12 to AF
-    MODIFY_FIELD(GPIOB->MODER, GPIO_MODER_MODER1, 0);
-    MODIFY_FIELD(GPIOB->MODER, GPIO_MODER_MODER11, 0);
-    MODIFY_FIELD(GPIOB->MODER, GPIO_MODER_MODER12, 0);
-
-    // Clock is divided by 16 (2^(BR+1))
-    MODIFY_FIELD(SPI1->CR1, SPI_CR1_BR, 3);
-    MODIFY_FIELD(SPI1->CR1, SPI_CR1_MSTR, 1); // Master Mode
-
-    // Select second edge sample, active high clock
-    MODIFY_FIELD(SPI1->CR1, SPI_CR1_CPHA, 1);
-    MODIFY_FIELD(SPI1->CR1, SPI_CR1_CPOL, 1);
-
-    // Data is MSB first
-    MODIFY_FIELD(SPI1->CR1, SPI_CR1_LSBFIRST, 0);
-
-    // SPI in motorola mode
-    MODIFY_FIELD(SPI1->CR2, SPI_CR2_FRF, 0);
-
-    // Enable SPI1
-    MODIFY_FIELD(SPI1->CR1, SPI_CR1_SPE, 1);
-}
-
-// #define F_TIM_CLOCK (48UL * 1000UL * 1000UL) // 48MHz
-// #define F_TIM_OVERVIEW (100UL * 1000UL)      // 100 kHz
-// #define TIM_PRESCALER (16)
-// #define TIM_PERIOD (F_TIM_CLOCK / (TIM_PRESCALER * F_TIM_OVERVIEW))
-
-// void Init_DMA(void)
-// {
-//     // Set up prescaler and counter to get 100 kHz
-//     RCC->AHB1ENR |= RCC_AHB1ENR_DMA2EN;
-//     TIM6->ARR = TIM_PERIOD - 1;
-//     // Enable DMA request on update
-//     TIM6->PSC = TIM_PRESCALER - 1;
-//     // Enable counting
-//     TIM6->DIER = TIM_DIER_UDE;
-// }
-
-// void Init_SPI2(void) {}
-
-// uint8_t SPI_Send_Recieve_Byte(uint8_t d_out)
-// {
-//     uint8_t d_in;
-//     // Wait until the transmitter buffer is empty
-//     while ((SPI1->SR & SPI_SR_TXE) == 0)
-//         ;
-
-//     // Transmit d_out
-//     // Must tell compiler to use a byte write (not half-word)
-//     // by casting SPI1->DR into a pointer to a byte (uint8_t)
-//     *((uint8_t *)&(SPI1->DR)) = d_out;
-//     // Wait until receiver is not empty
-//     while ((SPI->SR & SPI_SR_RXNE) == 0)
-//         ;
-//     // Get d_in
-//     d_in = (uint8_t)SPI1->DR;
-//     return d_in;
-// }
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI1_Init(void);
 
 int main(void)
 {
-    return 0;
+    HAL_Init();
+
+    SystemClock_Config();
+
+    MX_GPIO_Init();
+    MX_SPI1_Init();
+
+    while (1)
+    {
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
+    }
+}
+
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    /** Configure the main internal regulator output voltage
+     */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = 16;
+    RCC_OscInitStruct.PLL.PLLN = 336;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+    RCC_OscInitStruct.PLL.PLLQ = 2;
+    RCC_OscInitStruct.PLL.PLLR = 2;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+static void MX_SPI1_Init(void)
+{
+
+    /* USER CODE BEGIN SPI1_Init 0 */
+
+    /* USER CODE END SPI1_Init 0 */
+
+    /* USER CODE BEGIN SPI1_Init 1 */
+
+    /* USER CODE END SPI1_Init 1 */
+    /* SPI1 parameter configuration*/
+    hspi1.Instance = SPI1;
+    hspi1.Init.Mode = SPI_MODE_MASTER;
+    hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+    hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+    hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+    hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+    hspi1.Init.NSS = SPI_NSS_SOFT;
+    hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+    hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+    hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+    hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+    hspi1.Init.CRCPolynomial = 10;
+    if (HAL_SPI_Init(&hspi1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN SPI1_Init 2 */
+
+    /* USER CODE END SPI1_Init 2 */
+}
+
+static void MX_GPIO_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    /* USER CODE BEGIN MX_GPIO_Init_1 */
+    /* USER CODE END MX_GPIO_Init_1 */
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(GPIOA, LD2_Pin | ncs_sensor_Pin, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin : B1_Pin */
+    GPIO_InitStruct.Pin = B1_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : LD2_Pin ncs_sensor_Pin */
+    GPIO_InitStruct.Pin = LD2_Pin | ncs_sensor_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* USER CODE BEGIN MX_GPIO_Init_2 */
+    /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void)
+{
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1)
+    {
+    }
+    /* USER CODE END Error_Handler_Debug */
 }
